4a235bebb5c260e6647bf921791e53f716a8469a
a1234567877 <lovar20@yahoo.com>
2015-05-13 23:24:27 -0400

    Please Work

---
+++ src/main/java/babbies/mod/Leopold.java
@@ -0,0 +1,30 @@
+package babbies.mod;
+
+import babbies.mod.help.Reference;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+
+@Mod.EventHandler
+public void penInit (FMLPreInitializationEvent event)
+{
+	
+	
+}
+
+@Mod.EventHandler
+public void init(FMLInitializationEvent event)
+{
+	
+}
+
+@Mod.EventHandler
+public void init(FMLInitializationEvent event)
+{
+	
+	
+}
+
+public class Leopold 
+{
+	
+}
---
+++ src/main/java/babbies/mod/help/Reference.java
@@ -0,0 +1,9 @@
+package babbies.mod.help;
+
+public class Reference 
+{
+	public static final String MODID = "leopold";
+	public static final String NAME = "Leopold";
+	public static final String VERSION = "0.01";
+
+}
